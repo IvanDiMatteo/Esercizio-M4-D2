0467ca586ea2f6bca8e37dd674464eda4fabb2b1
IvanDiMatteo <maxmileditorno@gmail.com>
2023-05-22 03:24:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const musicData = (id)=> {
+    let promise= new Promise((resolve,reject)=> {
+        let request =  fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + id, { method: 'GET' });
+        setTimeout(()=>{
+            resolve(request);
+            reject("error");
+        },1000);
+ });
+    return promise;
+}
+
+musicData("eminem")
+.then((response)=> {response.json().then((data)=> {getAlbum(data, "eminem")})})
+.catch((error)=> {console.log(error)});
+
+musicData("metallica")
+.then((response)=>{ response.json().then((data)=>{getAlbum(data, "metallica")})})
+.catch((error)=>{console.log(error)});
+
+
+
+musicData("queen")
+.then((response)=>{ response.json().then((data)=>{getAlbum(data, "queen")})})
+.catch((error)=>{console.log(error)});
+
+
+
+function getAlbum(datas, name) {
+
+    let albums = document.querySelector(`div#${name}Section`);
+    let albumsData = datas.data;
+
+    for (let i = 0; i < 4; i++) {
+
+        let dadDiv = document.createElement("div");
+        albums.appendChild(dadDiv);
+
+        let albumTitle = document.createElement("h4");
+        albumTitle.innerText = albumsData[i].title;
+        dadDiv.appendChild(albumTitle);
+
+        let albumImg = document.createElement("img");
+        albumImg.src = albumsData[i].album.cover_medium;
+        dadDiv.appendChild(albumImg);
+        
+    }
+}
